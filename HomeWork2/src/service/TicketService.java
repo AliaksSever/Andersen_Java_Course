56578caedf9585306278b62d5eed962f511eb159
AliaksSever <alexey.severyn@gmail.com>
2024-07-26 17:34:57 +0300

    - ticketStorage now is static - static method getTicketById which returns new Ticket object if there's no object with id provided

--- HomeWork2/src/service/TicketService.java
+++ HomeWork2/src/service/TicketService.java
@@ -3,8 +3,10 @@ package service;
 import module.Ticket;
 
 public class TicketService {
+	
+	private static Ticket[] ticketStorage;
+	
 	public static void main(String[] args) {
-		Ticket[] ticketStorage;
 		
 		Ticket tOne = new Ticket();
 		Ticket tTwo = new Ticket("CarnegHall", (short)234, 1721643666);
@@ -18,7 +20,14 @@ public class TicketService {
 				1.5f
 				);
 		Ticket t4 = new Ticket();
-		Ticket t5 = new Ticket("MDHall", (short)544, 1721643666);
+		Ticket t5 = new Ticket(
+				(short) 112,
+				"MDHall", (short)544, 
+				1721643666,
+				false,
+				'A',
+				2f
+				);
 		Ticket t6 = new Ticket();
 		Ticket t7 = new Ticket();
 		Ticket t8 = new Ticket();
@@ -29,5 +38,14 @@ public class TicketService {
 				tOne,tTwo,tThree,t4,t5,t6,t7,t8,t9,t10
 		};
 		
+	}
+	
+	private static Ticket getTicketById (short id) {
+		for (int i=0; i<ticketStorage.length; i++) {
+			if (ticketStorage[i].id == id) {
+				return ticketStorage[i];
+			}
+		}
+		return new Ticket();
 	}
 }
