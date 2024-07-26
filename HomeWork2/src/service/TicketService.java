2edfd4d8cbc9b5aa952e88bb3845c24854eb2435
AliaksSever <alexey.severyn@gmail.com>
2024-07-26 17:16:14 +0300

    Ticket Storage: - array of Ticket objects created - new Ticket objects added

--- HomeWork2/src/service/TicketService.java
+++ HomeWork2/src/service/TicketService.java
@@ -4,6 +4,8 @@ import module.Ticket;
 
 public class TicketService {
 	public static void main(String[] args) {
+		Ticket[] ticketStorage;
+		
 		Ticket tOne = new Ticket();
 		Ticket tTwo = new Ticket("CarnegHall", (short)234, 1721643666);
 		Ticket tThree = new Ticket(
@@ -15,5 +17,17 @@ public class TicketService {
 				'C',
 				1.5f
 				);
+		Ticket t4 = new Ticket();
+		Ticket t5 = new Ticket("MDHall", (short)544, 1721643666);
+		Ticket t6 = new Ticket();
+		Ticket t7 = new Ticket();
+		Ticket t8 = new Ticket();
+		Ticket t9 = new Ticket("GreenHall", (short)956, 1721643666);
+		Ticket t10 = new Ticket();
+		
+		ticketStorage = new Ticket[] {
+				tOne,tTwo,tThree,t4,t5,t6,t7,t8,t9,t10
+		};
+		
 	}
 }
