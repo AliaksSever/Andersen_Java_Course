95175ce61911a1472471c1f7862d7be15ad01a5d
AliaksSever <alexey.severyn@gmail.com>
2024-08-07 18:41:58 +0300

    put method added to CustomArrayList

--- Storage/src/storage/CustomArrayList.java
+++ Storage/src/storage/CustomArrayList.java
@@ -1,5 +1,36 @@
 package storage;
+import java.util.Arrays;
 
-public class CustomArrayList {
+public class CustomArrayList<T> {
+    private T[] customArray = (T[]) new Object[10];
+    private int size = 0;
 
+    public void put(T o) {
+        customArray[size] = o;
+        size++;
+        checkArrayLength();
+    }
+
+    public void get(int index) {}
+
+    public void delete(int index) {}
+
+    public void checkArrayLength() {
+        if (size==customArray.length) {
+            T[] newCustomArray = (T[]) new Object[customArray.length+5];
+            for (int i = 0; i < customArray.length; i++) {
+                newCustomArray[i] = customArray[i];
+            }
+            customArray = newCustomArray;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(customArray);
+    }
+
+    public int size() {
+        return size;
+    }
 }
