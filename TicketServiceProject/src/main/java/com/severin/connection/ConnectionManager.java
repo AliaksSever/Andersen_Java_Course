988531e01fe83741c4bec02fa26d1bbbde2c8964
Alexey <alexey.severyn@gmail.com>
2024-08-15 23:15:02 +0300

    properties util

--- TicketServiceProject/src/main/java/com/severin/connection/ConnectionManager.java
+++ TicketServiceProject/src/main/java/com/severin/connection/ConnectionManager.java
@@ -4,16 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import com.severin.util.PropertiesUtil;
+
 public class ConnectionManager {
-    private final static String URL = "jdbc:postgresql://localhost:5432/my_ticket_service_db";
-    private final static String USERNAME = "postgres";
-    private final static String PASSWORD = "121212";
+    private final static String URL_KEY = "db.url";
+    private final static String USERNAME_KEY = "db.username";
+    private final static String PASSWORD_KEY = "db.password";
 
     private ConnectionManager() {}
 
     public static Connection open() {
         try {
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            return DriverManager.getConnection(
+                    PropertiesUtil.get(URL_KEY),
+                    PropertiesUtil.get(USERNAME_KEY),
+                    PropertiesUtil.get(PASSWORD_KEY)
+            );
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
