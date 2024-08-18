3cb67ca506058148b4ca6d1f3344e06d5f759db4
Alexey <alexey.severyn@gmail.com>
2024-08-18 21:12:10 +0300

    testing connection

--- TicketServiceProjectHibernate/src/main/java/com/severin/Main.java
+++ TicketServiceProjectHibernate/src/main/java/com/severin/Main.java
@@ -1,9 +1,15 @@
 package com.severin;
 
+import org.hibernate.Session;
+import com.severin.util.HibernateUtil;
+
 public class Main
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            System.out.println("Connection established");
+            HibernateUtil.close();
+        }
     }
 }
