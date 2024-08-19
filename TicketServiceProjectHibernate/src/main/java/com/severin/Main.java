317a83cfb48300562af3a5311a737aab53295020
Alexey <alexey.severyn@gmail.com>
2024-08-19 22:17:05 +0300

    testing User creation

--- TicketServiceProjectHibernate/src/main/java/com/severin/Main.java
+++ TicketServiceProjectHibernate/src/main/java/com/severin/Main.java
@@ -3,13 +3,27 @@ package com.severin;
 import org.hibernate.Session;
 import com.severin.util.HibernateUtil;
 
+import com.severin.entity.User;
+
+import java.sql.Timestamp;
+
 public class Main
 {
     public static void main( String[] args )
     {
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            System.out.println("Connection established");
-            HibernateUtil.close();
+
+            session.getTransaction().begin(); // open transaction
+
+
+            User user = new User();
+            user.setName("Alex3");
+
+
+            session.persist(user); //save changes in object
+            session.getTransaction().commit(); //commit changes and close transactions
+
+            HibernateUtil.close(); // close SessionFactory
         }
     }
 }
