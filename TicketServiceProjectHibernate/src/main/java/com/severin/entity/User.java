ad7c3e3b0a54530479dddf75c85600061433439d
Alexey <alexey.severyn@gmail.com>
2024-08-19 22:16:34 +0300

    created entity User class

---
+++ TicketServiceProjectHibernate/src/main/java/com/severin/entity/User.java
@@ -0,0 +1,24 @@
+package com.severin.entity;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "users", schema = "public", catalog = "postgres")
+@lombok.Getter
+@lombok.Setter
+@lombok.AllArgsConstructor
+@lombok.NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    @Column(name = "creation_date")
+    private final Timestamp creationDate = new Timestamp(System.currentTimeMillis());
+
+}
